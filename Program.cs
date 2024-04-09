81bb5bece8afda5497ff8c493cfc268ac7be8f60
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-04-09 22:24:17 +0300

    Serialization and deserialization logic was implemented

--- Program.cs
+++ Program.cs
@@ -1,2 +1,62 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+
+namespace SerializationDeserialization;
+
+internal class Program
+{
+    static void Main()
+    {
+        Person person = new Person() { Name = "Alex", Surname = "Great", Born = 356 };
+
+        SerializePersonProperties(person, "alex_great2.txt");
+    }
+
+    static Dictionary<string, string> ReadDataFile(string path)
+    {
+        string[] data = File.ReadAllLines(path);
+        Dictionary<string, string> properties = new Dictionary<string, string>();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            string[] splittedData = data[i].Split(':');
+            properties.Add(splittedData[0], splittedData[1]);
+        }
+
+        return properties;
+    }
+
+    static void SetPersonProperties(Person person, Dictionary<string, string> data)
+    {
+        var propertyInfos = person.GetType().GetProperties();
+
+        for (int i = 0; i < propertyInfos.Length; i++)
+        {
+            if (data.ContainsKey(propertyInfos[i].Name))
+            {
+                try
+                {
+                    Type propertyType = propertyInfos[i].PropertyType;
+                    var convertedValue = Convert.ChangeType(data[propertyInfos[i].Name], propertyType);
+                    propertyInfos[i].SetValue(person, convertedValue, null);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
+    static void SerializePersonProperties(Person person, string filePath)
+    {
+        var properties = person.GetType().GetProperties();
+        var stringBuilder = new StringBuilder();
+
+        foreach (var property in properties)
+        {
+            stringBuilder.AppendLine($"{property.Name}:{property.GetValue(person, null)}");
+        }
+
+        File.AppendAllText(filePath, stringBuilder.ToString());
+    }
+}
