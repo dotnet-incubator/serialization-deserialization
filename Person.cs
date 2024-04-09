407c035992a152f11f39a50642561ca5c67321d9
Yauheni Kuntsevich <jauhien.k@gmail.com>
2024-04-09 22:21:30 +0300

    Person class was created

---
+++ Person.cs
@@ -0,0 +1,9 @@
+﻿namespace SerializationDeserialization;
+
+[Serializable]
+public class Person
+{
+    public string Name { get; set; } = null!;
+    public string Surname { get; set; } = null!;
+    public int Born { get; set; }
+}
